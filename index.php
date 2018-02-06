e0900e22ec1f42241e645d20ed5fd1258db522f4
Dylan Drexler <700214954@fvtc.edu>
2018-02-06 17:32:33 -0600

    Added intro and image and some CSS

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dylan Drexer Home Page-PHP</title>
+    <link rel="stylesheet" href=css/base.css>
+</head>
+
+    <div id="intro">
+        <header>
+            <h1>Welcome To My Course Home Page</h1>
+            <h2>My name is Dylan Drexler</h2>
+        </header>
+        <section id="about-myself">
+            <article>
+                <img src="img/Fish.JPG" alt="Fish">
+                <h1>A Little About Myself</h1>
+                <p>Hello everyone, my name is Dylan I'm 18 years old, and i was born an raised in Oshkosh WI. I spend a lot of my free time either fishing or just spending time with friends and family, i used to play football and baseball when i was younger but i fell out of it when i came into high school.I am currently a student at FVTC just like all of you, my program is IT Web Development & Design and that is my only program. I am in my second semester and I'm really enjoying it so far, and so excited for what is to come. In my program i am working toward my associates degree, i still have not  decided if i want to continue my schooling after my program is complete here or to transfer credits to a university, but i do know that i will stick in the IT field for the rest of my life.
+                </p>
+            </article>
+        </section>
+    </div>
+
+
+</html>
